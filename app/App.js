48e6a0ad882158945b1eeb5f5a3b4948660c343f
Guilherme Costa <guilhermemasterts@gmail.com>
2020-11-22 00:24:10 -0300

    Build: Estrutura de pastas

--- app/App.js
+++ app/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
+import * as theme from './styles/theme';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My Books</Text>
+      <Text style={{ ...theme.fonts.h1 }}>My Books</Text>
     </View>
   )
 }
---
+++ app/styles/theme.js
@@ -0,0 +1,23 @@
+const colors = {
+  primary: '#CAF5EC',
+  black: '#344846',
+  white: '#FFFFFF',
+  red: '#FC6681',
+  gray: 'rgba(50, 56, 66, 0.6)'
+}
+
+const sizes = {
+  monster: 40,
+  h1: 24,
+  h2: 16,
+  text: 16
+}
+
+const fonts = {
+  h1: {
+    fontFamily: 'Kanit-Bold',
+    fontSize: sizes.h2
+  }
+}
+
+export { colors, sizes, fonts }
